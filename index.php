f514c39e66bac820fbe44effed2ca3f7f3ea72a3
Oliwer Banach <oliwerbanach@gmail.com>
2023-03-08 01:00:56 +0100

    Create Customer.php + php update

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-
+require_once('vendor/autoload.php');
 
 use Zen\Model\ZenCredentials;
-
-require_once('vendor/autoload.php');
 
 
 $zenCredentials = new ZenCredentials('$SECRET_KEY', '$IPN_SECRET', '$TERMINAL_UUID');
---
+++ src/Model/Payment/Customer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Zen\Model\Payment;
+
+class Customer {
+
+    private string $firstName;
+    private string $lastName;
+    private string $email;
+
+    public function __construct($firstName, $lastName, $email){
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+}
